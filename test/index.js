fabf5ba3e028db7db72e7661b0e4631af6f4e794
sokainaslassi <sokaina.slassi@gmail.com>
2015-12-01 18:10:28 +0100

    commi slassi

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
-import assert from 'assert';
+import { should } from 'chai';
 import catatonicCircle from '../lib';
 
-describe('Catatonic-circle', function () {
+should(); // Modifies objects prototype to include the 'should' property
+
+describe('catatonic-circle', function () {
   it('should have a version number!', function () {
-    assert(typeof catatonicCircle.VERSION !== 'undefined', 'The Project should have a VERSION, whatever the actual version.');
+    catatonicCircle.should.have.property('VERSION');
   });
 });
-
-
